736327261a68dce2fa1c1f8dca8fb55c6557a202
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2024-01-16 13:24:59 +0530

    .

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,25 @@ const Secret = require("../model/user")
 const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
-router.post('/signup',async function(req, res, next) {
-  const newUser=new Secret({
-    UID:req.body.uid,
-    Email:req.body.email,
-    Name:req.body.name
-  })
-  await newUser.save();
-  const user=await Secret.findOne({UID:req.body.uid});
-  res.send(jwt.sign({id:user._id,token:user.UID,name:user.Name}, process.env.TOKEN_SECRET));
+router.post('/signup', async function (req, res, next) {
+  const prevuser = await Secret.findOne({ UID: req.body.uid });
+  if (!prevuser) {
+    const newUser = new Secret({
+      UID: req.body.uid,
+      Email: req.body.email,
+      Name: req.body.name
+    })
+    await newUser.save();
+    const user = await Secret.findOne({ UID: req.body.uid });
+    return res.send(jwt.sign({ id: user._id, token: user.UID, name: user.Name }, process.env.TOKEN_SECRET));
+  }
+  return res.send(jwt.sign({ id: prevuser._id, token: prevuser.UID, name: prevuser.Name }, process.env.TOKEN_SECRET));
+
 });
 
-router.post('/login',async function(req, res, next) {
-  const user=await Secret.findOne({UID:req.body.uid});
-  res.send(jwt.sign({id:user._id,token:user.UID,name:user.Name}, process.env.TOKEN_SECRET));
+router.post('/login', async function (req, res, next) {
+  const user = await Secret.findOne({ UID: req.body.uid });
+  res.send(jwt.sign({ id: user._id, token: user.UID, name: user.Name }, process.env.TOKEN_SECRET));
 });
 
 module.exports = router;
