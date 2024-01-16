3add6203dedc9b946c30c0309a82eafafe4e52b9
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2024-01-16 13:35:53 +0530

    m

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 require("./mongooConnect")
 const dotenv = require('dotenv');
+const cors=require("cors")
 dotenv.config();
 
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
